750fdf61f116468f5ac8a536ae7d9183127e327d
James <jgebczak@gmail.com>
2014-07-14 00:39:45 -0700

    Add comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var http = require("http");
 
+// When a server receives a request, it will fire a function(request,response) provided to http.createServer.
+// My code extends the "request" part of it, so we can get passed query params simply by calling .param(name) on it.
+
 http.IncomingMessage.prototype.param = function(name)  {
 
   var url = require('url');
